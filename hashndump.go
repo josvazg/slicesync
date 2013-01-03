dcbdcdb3259307f33db45f6a4ad806469192bf32
josvazg (Jose Luis Vazquez González) <josvazg@gmail.com>
2013-01-04 00:24:52 +0100

    Compiles but no tiem to test it

--- hashndump.go
+++ hashndump.go
@@ -3,15 +3,15 @@ package slicesync
 import (
 	"crypto/sha1"
 	"fmt"
-	"net/http"
 	"io"
+	"net/http"
 	"os"
 	"path"
 	"strconv"
 )
 
 const (
-	AUTOSIZE=0
+	AUTOSIZE = 0
 )
 
 type LimitedReadCloser struct {
@@ -77,86 +77,103 @@ func sliceFile(file *os.File, offset, size int64) (int64, error) {
 func SetupHashNDump() {
 	http.HandleFunc("/hash", hash)
 	http.HandleFunc("/dump", dump)
+	http.HandleFunc("/size", size)
 }
 
 // HashNDumpServer prepares an HTTP Server to Hash and Dump slices of files remotely
 func HashNDumpServer(port int) {
 	SetupHashNDump()
-	http.ListenAndServe(fmt.Sprintf(":%v",port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 }
 
 // hash is a http request handler to return hashes of file slices
 func hash(w http.ResponseWriter, r *http.Request) {
-	filename,offset,size,ok:=readArgs(w,r)
-	if !ok {
+	filename, offset, size, err := readArgs(w, r)
+	if handleError(w, r, err) {
 		return
 	}
-	hsh,err:=Hash(filename,offset,size)
-	if handleError(w,r,err) {
+	hsh, err := Hash(filename, offset, size)
+	if handleError(w, r, err) {
 		return
 	}
-	io.WriteString(w,hsh)
+	io.WriteString(w, hsh)
 }
 
 // dump is a http request handler to return a file slice
 func dump(w http.ResponseWriter, r *http.Request) {
-	filename,offset,size,ok:=readArgs(w,r)
-	if !ok {
+	filename, offset, size, err := readArgs(w, r)
+	if handleError(w, r, err) {
 		return
 	}
-	sliced:=!(offset==0 && size==0)
-	slice,size,err:=Dump(filename,offset,size)
-	if handleError(w,r,err) {
+	sliced := !(offset == 0 && size == 0)
+	slice, size, err := Dump(filename, offset, size)
+	if handleError(w, r, err) {
 		return
-	}	
-	w.Header().Set("Content-Length",fmt.Sprintf("%v",size))
-	w.Header().Set("Content-Type","application/octet-stream")
-	downfilename:=filename
+	}
+	w.Header().Set("Content-Length", fmt.Sprintf("%v", size))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	downfilename := filename
 	if sliced {
-		downfilename=fmt.Sprintf("%s(%v-%v)%s",noExt(filename),offset,size,path.Ext(filename))
+		downfilename = fmt.Sprintf("%s(%v-%v)%s", noExt(filename), offset, size, path.Ext(filename))
 	}
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"",downfilename))
-	io.Copy(w,slice)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", downfilename))
+	io.Copy(w, slice)
+}
+
+// size is a http request handler to return a file size
+func size(w http.ResponseWriter, r *http.Request) {
+	filename, _, _, err := readArgs(w, r)
+	if handleError(w, r, err) {
+		return
+	}
+	file, err := os.Open(filename) // For read access.
+	if handleError(w, r, err) {
+		return
+	}
+	fi, err := file.Stat()
+	if handleError(w, r, err) {
+		return
+	}
+	io.WriteString(w,fmt.Sprintf("%v",fi.Size()))
 }
 
 // noExt returns the name without the extension
 func noExt(filename string) string {
-	return filename[0:len(filename)-len(path.Ext(filename))]
+	return filename[0 : len(filename)-len(path.Ext(filename))]
 }
 
 // readArgs reads request args for hash & dump
-func readArgs(w http.ResponseWriter, r *http.Request) (f string, o, s int64, ok bool) {
-filename:=r.FormValue("filename")
-	if filename=="" {
-		handleError(w,r,fmt.Errorf("Expected filename argument!"))
-		return "",0,0,false
+func readArgs(w http.ResponseWriter, r *http.Request) (f string, o, s int64, e error) {
+	filename := r.FormValue("filename")
+	if filename == "" {
+		return "", 0, 0, fmt.Errorf("Expected filename argument!")
 	}
-	offset:=r.FormValue("offset")
-	size:=r.FormValue("size")
-	o=0
-	s=AUTOSIZE
-	if offset!="" {
-		i,err:=strconv.ParseInt(offset,10,64)
-		if handleError(w,r,err) {
-			return "",0,0,false
+	offset := r.FormValue("offset")
+	size := r.FormValue("size")
+	o = 0
+	s = AUTOSIZE
+	if offset != "" {
+		i, err := strconv.ParseInt(offset, 10, 64)
+		if err!=nil {
+			return "", 0, 0, err
 		}
-		o=i
+		o = i
 	}
-	if size!="" {
-		i,err:=strconv.ParseInt(size,10,64)
-		if handleError(w,r,err) {
-			return "",0,0,false
+	if size != "" {
+		i, err := strconv.ParseInt(size, 10, 64)
+		if err!=nil {
+			return "", 0, 0, err
 		}
-		s=i
+		s = i
 	}
-	return filename,o,s,true
+	return filename, o, s, nil
 }
 
 // handleError displays err (if not nil) on Stderr and (if possible) displays a web error page
 // it also returns true if the error was found and handled and false if err was nil
 func handleError(w http.ResponseWriter, r *http.Request, err error) bool {
-	if err!=nil {
-		fmt.Fprintln(os.Stderr,err.Error())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return true
 	}
