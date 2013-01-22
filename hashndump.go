289d5371d173e8ad36c1cb3910e0cbfcba0c5f7b
josvazg (Jose Luis Vazquez González) <josvazg@gmail.com>
2013-01-22 23:35:50 +0100

    Compiles and now I need a full client-server test

--- hashndump.go
+++ hashndump.go
@@ -4,10 +4,7 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"io"
-	"net/http"
 	"os"
-	"path"
-	"strconv"
 )
 
 const (
@@ -18,44 +15,60 @@ type LimitedReadCloser struct {
 	io.LimitedReader
 }
 
+// FileInfo info to be sent back
+type FileInfo struct {
+	size   int64
+	offset int64
+	slice  int64
+	hash   string
+}
+
 func (l *LimitedReadCloser) Close() error {
 	return (l.R).(io.Closer).Close()
 }
 
 // Hash returns the Hash (sha-1) for a file slice or the full file
-func Hash(filename string, offset, size int64) (string, error) {
+func Hash(filename string, offset, slice int64) (*FileInfo, error) {
 	file, err := os.Open(filename) // For read access.
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer file.Close()
-	size, err = sliceFile(file, offset, size)
+	fi, err := file.Stat()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	h := sha1.New()
-	io.CopyN(h, file, size)
-	return "sha1-" + fmt.Sprintf("%x", h.Sum(nil)), nil
+	slice, err = sliceFile(file, offset, slice)
+	if err != nil {
+		return nil, err
+	}
+	hash := ""
+	if slice > 0 {
+		h := sha1.New()
+		io.CopyN(h, file, slice)
+		hash = "sha1-" + fmt.Sprintf("%x", h.Sum(nil))
+	}
+	return &FileInfo{fi.Size(), offset, slice, hash}, nil
 }
 
 // Dump opens a file to read just a slice of it
-func Dump(filename string, offset, size int64) (io.ReadCloser, int64, error) {
+func Dump(filename string, offset, slice int64) (io.ReadCloser, int64, error) {
 	file, err := os.Open(filename) // For read access.
 	if err != nil {
 		return nil, 0, err
 	}
-	size, err = sliceFile(file, offset, size)
+	slice, err = sliceFile(file, offset, slice)
 	if err != nil {
 		return nil, 0, err
 	}
-	return &LimitedReadCloser{io.LimitedReader{file, size}}, size, nil
+	return &LimitedReadCloser{io.LimitedReader{file, slice}}, slice, nil
 }
 
-// sliceFile positions to the offset pos of file and prepares to read up to size bytes of it.
-// It returns the proper size to read before the end of the file is reached: 
-// When input size is 0 or size would read past the file's end, it returns the remaining length 
+// sliceFile positions to the offset pos of file and prepares to read up to slice bytes of it.
+// It returns the proper slice to read before the end of the file is reached: 
+// When input slice is 0 or slice would read past the file's end, it returns the remaining length 
 // to read before EOF
-func sliceFile(file *os.File, offset, size int64) (int64, error) {
+func sliceFile(file *os.File, offset, slice int64) (int64, error) {
 	if offset > 0 {
 		_, err := file.Seek(offset, os.SEEK_SET)
 		if err != nil {
@@ -67,119 +80,8 @@ func sliceFile(file *os.File, offset, size int64) (int64, error) {
 		return 0, err
 	}
 	max := fi.Size()
-	if size == 0 || (offset+size) > max {
-		size = max - offset
+	if slice == 0 || (offset+slice) > max {
+		slice = max - offset
 	}
-	return size, nil
-}
-
-// HashNDumpServer prepares an HTTP Server to Hash and Dump slices of files remotely
-func SetupHashNDump() {
-	http.HandleFunc("/", dump)
-	http.HandleFunc("/favicon.ico",http.NotFound)
-	http.Handle("/hash/", http.StripPrefix("/hash/",http.HandlerFunc(hash)))
-	http.Handle("/size/", http.StripPrefix("/size/", http.HandlerFunc(size)))
-}
-
-// HashNDumpServer prepares an HTTP Server to Hash and Dump slices of files remotely
-func HashNDumpServer(port int) {
-	SetupHashNDump()
-	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
-}
-
-// hash is a http request handler to return hashes of file slices
-func hash(w http.ResponseWriter, r *http.Request) {
-	filename, offset, size, err := readArgs(w, r)
-	if handleError(w, r, err) {
-		return
-	}
-	hsh, err := Hash(filename, offset, size)
-	if handleError(w, r, err) {
-		return
-	}
-	io.WriteString(w, hsh)
-}
-
-// dump is a http request handler to return a file slice
-func dump(w http.ResponseWriter, r *http.Request) {
-	filename, offset, size, err := readArgs(w, r)
-	if handleError(w, r, err) {
-		return
-	}
-	sliced := !(offset == 0 && size == 0)
-	slice, size, err := Dump(filename, offset, size)
-	if handleError(w, r, err) {
-		return
-	}
-	w.Header().Set("Content-Length", fmt.Sprintf("%v", size))
-	w.Header().Set("Content-Type", "application/octet-stream")
-	downfilename := filename
-	if sliced {
-		downfilename = fmt.Sprintf("%s(%v-%v)%s", noExt(filename), offset, size, path.Ext(filename))
-	}
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", downfilename))
-	io.Copy(w, slice)
-}
-
-// size is a http request handler to return a file size
-func size(w http.ResponseWriter, r *http.Request) {
-	filename, _, _, err := readArgs(w, r)
-	if handleError(w, r, err) {
-		return
-	}
-	file, err := os.Open(filename) // For read access.
-	if handleError(w, r, err) {
-		return
-	}
-	fi, err := file.Stat()
-	if handleError(w, r, err) {
-		return
-	}
-	io.WriteString(w, fmt.Sprintf("%v\n", fi.Size()))
-}
-
-// noExt returns the name without the extension
-func noExt(filename string) string {
-	return filename[0 : len(filename)-len(path.Ext(filename))]
-}
-
-// readArgs reads request args for hash & dump
-func readArgs(w http.ResponseWriter, r *http.Request) (f string, o, s int64, e error) {
-	filename := r.URL.Path
-	if filename[0]=='/' {
-		filename=filename[1:]
-	}
-	if filename == "" {
-		return "", 0, 0, fmt.Errorf("Expected filename argument!")
-	}
-	offset := r.FormValue("offset")
-	size := r.FormValue("size")
-	o = 0
-	s = AUTOSIZE
-	if offset != "" {
-		i, err := strconv.ParseInt(offset, 10, 64)
-		if err != nil {
-			return "", 0, 0, err
-		}
-		o = i
-	}
-	if size != "" {
-		i, err := strconv.ParseInt(size, 10, 64)
-		if err != nil {
-			return "", 0, 0, err
-		}
-		s = i
-	}
-	return filename, o, s, nil
-}
-
-// handleError displays err (if not nil) on Stderr and (if possible) displays a web error page
-// it also returns true if the error was found and handled and false if err was nil
-func handleError(w http.ResponseWriter, r *http.Request, err error) bool {
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return true
-	}
-	return false
+	return slice, nil
 }
