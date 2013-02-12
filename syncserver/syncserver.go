4ee07dac48cf21586953ff920f2deeee922deadd
Jose Luis Vázquez González <josvazg@gmail.com>
2013-02-12 16:59:49 +0100

    Port is configurable by a flag

--- syncserver/syncserver.go
+++ syncserver/syncserver.go
@@ -2,12 +2,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/josvazg/slicesync"
 )
-
+// Start the server on port 8000 by default
 func main() {
-	port:=8080
+	var port int
+	flag.IntVar(&port,"port",8000,"Server port to listen from")
+    flag.Parse()
 	fmt.Printf("slicesync server (Hash&Dump) listening at %v...\n", port)
 	slicesync.HashNDumpServer(port)
 }
