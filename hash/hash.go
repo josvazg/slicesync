f300dffd1b305ce8e8171af1ef68900e95efb71a
Jose Luis Vázquez González <josvazg@gmail.com>
2013-02-18 13:17:54 +0100

    Portable Hash calculus

---
+++ hash/hash.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/josvazg/slicesync"
+	"os"
+)
+
+const (
+	MiB = 1048576
+)
+
+func toMiB(bytes int64) float64 {
+	return float64(bytes) / MiB
+}
+
+func pct(bytes, total int64) float64 {
+	return float64(bytes*100) / float64(total)
+}
+
+func main() {
+	if(len(os.Args)<2) {
+		fmt.Printf("Usage: %d {filename}\n",os.Args[0])
+		return
+	}
+	filename:=os.Args[1]
+	fi,err:=slicesync.Hash(filename,0,slicesync.AUTOSIZE)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error()+"\n")
+		return
+	}
+	fmt.Println(fi)
+}
